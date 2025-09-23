76314b5078b8ff6195cf771d3cadeb9bd3be8eb4
untiager <you@example.com>
2025-09-23 13:43:30 +0200

    [~]file format change

--- src/Utils/ConsoleErrorHandler.cpp
+++ src/Utils/ConsoleErrorHandler.cpp
@@ -9,18 +9,21 @@ ConsoleErrorHandler::ConsoleErrorHandler(EventDispatcher* dispatcher)
 {
 }
 
-void ConsoleErrorHandler::handleError(const std::string& message) {
+void ConsoleErrorHandler::handleError(const std::string& message)
+{
     std::cerr << "[ERROR] " << message << std::endl;
     if (eventDispatcher_) {
         eventDispatcher_->emit<ErrorEvent>(message);
     }
 }
 
-void ConsoleErrorHandler::handleWarning(const std::string& message) {
+void ConsoleErrorHandler::handleWarning(const std::string& message)
+{
     std::cout << "[WARNING] " << message << std::endl;
 }
 
-void ConsoleErrorHandler::handleInfo(const std::string& message) {
+void ConsoleErrorHandler::handleInfo(const std::string& message)
+{
     std::cout << "[INFO] " << message << std::endl;
 }
 
