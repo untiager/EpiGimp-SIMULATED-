29bf9d0085b754bbf55a5f81999f47f96f5e57a6
untiager <you@example.com>
2025-09-23 14:11:31 +0200

    [~]file format change

--- src/Utils/RaylibInputHandler.cpp
+++ src/Utils/RaylibInputHandler.cpp
@@ -4,35 +4,43 @@
 
 namespace EpiGimp {
 
-void RaylibInputHandler::update() {
+void RaylibInputHandler::update()
+{
     // Raylib handles input automatically, so nothing to do here
 }
 
-bool RaylibInputHandler::isKeyPressed(int key) const {
+bool RaylibInputHandler::isKeyPressed(int key) const
+{
     return IsKeyPressed(key);
 }
 
-bool RaylibInputHandler::isKeyDown(int key) const {
+bool RaylibInputHandler::isKeyDown(int key) const
+{
     return IsKeyDown(key);
 }
 
-bool RaylibInputHandler::isMouseButtonPressed(int button) const {
+bool RaylibInputHandler::isMouseButtonPressed(int button) const
+{
     return IsMouseButtonPressed(button);
 }
 
-bool RaylibInputHandler::isMouseButtonDown(int button) const {
+bool RaylibInputHandler::isMouseButtonDown(int button) const
+{
     return IsMouseButtonDown(button);
 }
 
-Vector2 RaylibInputHandler::getMousePosition() const {
+Vector2 RaylibInputHandler::getMousePosition() const
+{
     return GetMousePosition();
 }
 
-Vector2 RaylibInputHandler::getMouseDelta() const {
+Vector2 RaylibInputHandler::getMouseDelta() const
+{
     return GetMouseDelta();
 }
 
-float RaylibInputHandler::getMouseWheelMove() const {
+float RaylibInputHandler::getMouseWheelMove() const
+{
     return GetMouseWheelMove();
 }
 
